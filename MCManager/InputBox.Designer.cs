d526d83ceabefa9e8a196e64d3142d7525478816
Jenjen1324 <northcodedotno@gmail.com>
2013-02-16 22:51:35 +0100

    Tried to fix bugs

--- MCManager/InputBox.Designer.cs
+++ MCManager/InputBox.Designer.cs
@@ -53,6 +53,7 @@
             // 
             // button1
             // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.button1.Location = new System.Drawing.Point(12, 63);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
