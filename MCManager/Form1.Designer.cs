dd80939d0f5422b7808169a4fcf6b2cc6774c1d4
Northcode <northcode.no@gmail.com>
2013-02-18 14:13:04 +0100

    rebuild

--- MCManager/Form1.Designer.cs
+++ MCManager/Form1.Designer.cs
@@ -348,9 +348,9 @@
             // 
             this.button3.Location = new System.Drawing.Point(234, 91);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(138, 23);
             this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
+            this.button3.Text = "Start without Launcher";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
