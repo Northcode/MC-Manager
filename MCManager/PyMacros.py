8c3caaf943c16b3e26b0baf6577f502c860d9629
Northcode <northcode.no@gmail.com>
2013-02-23 22:07:44 +0100

    Added PyMacros for vs python tools

---
+++ MCManager/PyMacros.py
@@ -0,0 +1,14 @@
+﻿#TO USE THIS INSTALL IRONPYTHON AND VISUAL STUDIO PYTHON TOOLS
+#YOU NEED VS FULL EDITION!
+
+#use: from PyMacros import *
+#to use in python interactive in VS
+
+def setver(ver):
+    """Sets the version of MCManager both in version.cs and ver.txt"""
+    cfs = open("version.cs","w")
+    cfs.write("namespace MCManager\n{\class v\n{\npublic const string G_VERSION = \"" + ver + "\";\n}\n}")
+    cfs.close()
+    tfs = open("ver.txt","w")
+    tfs.write(ver)
+    tfs.close()
